55e6cc5b227975f6680ebfb56a484e7ba7abc73c
SzilagyiZsolt <zsoltika20030@gmail.com>
2024-03-12 08:59:02 +0100

    2024.03.12

--- Grafikus rest api/Form1.Designer.cs
+++ Grafikus rest api/Form1.Designer.cs
@@ -28,13 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listbox_adatok = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listbox_adatok
+            // 
+            this.listbox_adatok.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listbox_adatok.FormattingEnabled = true;
+            this.listbox_adatok.Location = new System.Drawing.Point(0, 0);
+            this.listbox_adatok.Name = "listbox_adatok";
+            this.listbox_adatok.Size = new System.Drawing.Size(120, 450);
+            this.listbox_adatok.TabIndex = 0;
+            this.listbox_adatok.SelectedIndexChanged += new System.EventHandler(this.adatok_listbox_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listbox_adatok);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listbox_adatok;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- Grafikus rest api/Form1.cs
+++ Grafikus rest api/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Grafikus_rest_api;
 
 namespace Grafikus_rest_api
 {
@@ -15,6 +17,39 @@ namespace Grafikus_rest_api
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            await dolgozok();
+            adatok();
+        }
+        private static async Task<List<Api>> dolgozok()
+        {
+            List<Api> nyil = new List<Api>();
+            string url = $"https://retoolapi.dev/Kc6xuH/data";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                nyil = Api.FromJson(jsonString).ToList();
+            }
+            return nyil;
+        }
+
+        private void adatok_listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+        private void adatok()
+        {
+            List<Api> nyil = new List<Api>();
+            foreach (Api api in nyil)
+            {
+                listbox_adatok.Items.Add(api.ApiName);
+            }
         }
     }
 }
